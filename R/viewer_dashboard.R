076568c895f12dd257ac1029493901ac86c6f8b4
Farid Azouaou <doudou_ulb@hotmail.com>
2022-07-08 23:51:43 +0200

    dashboard view bugfix

---
+++ R/viewer_dashboard.R
@@ -0,0 +1,9 @@
+#' run_viewer_dashboard
+#'@description a flexdashboard which will be used for viewer user without a need to operate any changes
+#'@param target_dashboard the dashboard for which they should access to
+#'@export
+run_viewer_dashboard <- function(target_dashboard = "viewer_dashboard"){
+  target_dashboard <- paste0("dashboards/",target_dashboard,".Rmd")
+  viewer_dashboard_file <- system.file(target_dashboard, package = "SupplAI")   
+  rmarkdown::run(file = viewer_dashboard_file, shiny_args = list(port = 3838, host = '0.0.0.0'))
+}
